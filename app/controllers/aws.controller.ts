f1460eb8a9dad52a8a8ddb94dadad19bf5c8a460
samueleastdev <mail@samueleast.co.uk>
2024-01-31 09:47:43 +0000

    after removing mulitpart upload removal

--- app/controllers/aws.controller.ts
+++ app/controllers/aws.controller.ts
@@ -7,9 +7,6 @@ import {
   DeleteObjectCommand,
   HeadObjectCommand,
   ListObjectsV2Command,
-  CreateMultipartUploadCommand,
-  CompleteMultipartUploadCommand,
-  UploadPartCommand,
   GetObjectCommand,
 } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
